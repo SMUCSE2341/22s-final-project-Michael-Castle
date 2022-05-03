0051a0ee6d1e9331ddc2231df30ca53f47c2b2a5
willk <wkornriech@smu.edu>
2022-05-03 01:16:15 -0500

    added DSDocument class

---
+++ DSDocument.h
@@ -0,0 +1,28 @@
+//
+// Created by willk on 5/3/2022.
+//
+#ifndef string
+#include <string>
+#include <cstring>
+
+#endif
+
+#ifndef INC_22S_FINAL_PROJ_DSDOCUMENT_H
+#define INC_22S_FINAL_PROJ_DSDOCUMENT_H
+
+
+
+
+using namespace std;
+class DSDocument {
+public:
+    const char *ID;
+    string text;
+    bool operator<(const DSDocument& input) {
+        return ID<input.ID;
+    }
+
+
+};
+
+#endif //INC_22S_FINAL_PROJ_DSDOCUMENT_H
